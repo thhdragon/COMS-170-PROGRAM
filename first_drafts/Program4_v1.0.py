d8f80575a5bb77da0f0d5e48a087c884fc7050d5
thhdragon <46762155+thhdragon@users.noreply.github.com>
2026-02-21 20:02:12 -0500

    refactor dice rolling logic to improve variable usage and enhance output clarity

--- first_drafts/Program4_v1.0.py
+++ first_drafts/Program4_v1.0.py
@@ -16,27 +16,40 @@ from random import randint
 # define magic number constants
 GOAL: int = 12
 # init variables to 0
+sum_of_current_roll: int = 0
+sum_of_all_rolls: int = 0
 current_roll_count: int = 0
-sum_all_rolls: int = 0
-roll_total: int = 0
+
 # create intro
 INTRO_MSG: str = f"{'*' * 28}\n*{' ' * 4}Random Dice Roller{' ' * 4}*\n{'*' * 28}"
-
 # print intro
 print(INTRO_MSG)
 
-# Roll dice
-while roll_total != GOAL:
+# main loop to roll dice
+# while sum of current roll is not equal to GOAL
+# while die 1 + die 2 is not 12
+while sum_of_current_roll != GOAL:
+    # use the randint method from the random module to get random values for die_1 and die_2
     die_1: int = randint(1, 6)
     die_2: int = randint(1, 6)
-    roll_total: int = die_1 + die_2
-    current_roll_count += 1
-    sum_all_rolls += roll_total
-    print(f"{current_roll_count:2}.)  Dice: {die_1} - {die_2}  Total: {roll_total}")
 
-average_of_roll_sum_total: float = sum_all_rolls / current_roll_count
+    # Add die to get sum of current roll
+    sum_of_current_roll: int = die_1 + die_2
+
+    # increment the counter that says we just did a "virtual" dice roll.
+    current_roll_count += 1
+
+    # add the sum of the current roll to the total sum from all rolls
+    # sum total = sum total + sum of new roll
+    sum_of_all_rolls += sum_of_current_roll
+
+    # print out per roll results
+    print(f"{current_roll_count:2}.)  Dice: {die_1} - {die_2}  Total: {sum_of_current_roll}")
+
+# after the loop condition becomes valid get the average from dividing sum of all rolls and roll count
+average_from_roll_sum_total: float = sum_of_all_rolls / current_roll_count
 print(f"Rolls:   {current_roll_count}")
-print(f"Average: {average_of_roll_sum_total:.2f}")
+print(f"Average: {average_from_roll_sum_total:.2f}")
 
 
 # ----------------------------------------
